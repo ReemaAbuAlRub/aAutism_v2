bb6956655ea4bc63149cc05cc4d58f9c3806b913
ReemaAbuAlRub <reema_maen@hotmail.com>
2025-08-06 17:51:47 +0300

    fourth commit

--- backend/app/services/llm/factories.py
+++ backend/app/services/llm/factories.py
@@ -61,26 +61,13 @@ class OPenAIFactory(BaseLlmFactory):
 
 class GoogleFactory(BaseLlmFactory):
     def __init__(self):
-        self.client = AsyncOpenAI(api_key=settings.OPENAI_API_KEY)
-        self.model = "gpt-4.1"
-        self.temperature = 0.2
+        pass
 
     async def generate(self, message: str, autism_level :str, history: list[dict] = None) -> str:
-        messages=build_messages(
-            user=message,
-            history=history,
-            level=autism_level
-        )
-        response = await self.client.chat.completions.create(
-            model=self.model,
-            messages=messages,
-            temperature=self.temperature,
-        )
-        return response.choices[0].message.content
+        pass
 
     async def get_embedding( self, text: str) -> list[float]:
-        resp = await self.client.embeddings.create( model=settings.EMBEDDING_MODEL, input=text )
-        return resp.data[0].embedding
+        pass
 
 
 class LLMFactory:
